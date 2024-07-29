8ff6fbee22a0e9d8adc36f68489d6e3627af5353
John Yu <junye.yu.2@gmail.com>
2024-07-29 16:01:10 -0600

    Optimized the sql query using the OUTPUT keyword

--- CleanStagingData.sql
+++ CleanStagingData.sql
@@ -5,8 +5,7 @@ GO
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'stg.Errors') AND type in (N'U'))
 BEGIN
 	CREATE TABLE stg.Errors (
-		ErrorID INT IDENTITY(1,1) PRIMARY KEY,
-		StgID INT,
+		StgID INT PRIMARY KEY,
 		UserID INT,
 		FullName NVARCHAR(255),
 		Age INT,
@@ -35,6 +34,7 @@ BEGIN
 	DELETE FROM stg.Users
 	WHERE StgID IN (SELECT StgID FROM CTE_Duplicates WHERE RowNum > 1);
 
+	-- Declare variables to be used to find purchaseTotals that are outliers.
 	DECLARE
 		@Q1 FLOAT,
 		@Q3 FLOAT,
@@ -57,42 +57,19 @@ BEGIN
 	SET @LowerLimit = @Q1 - 1.5 * @IQR;
 	SET @UpperLimit = @Q3 + 1.5 * @IQR;
 
-	-- Insert records to Errors Table
-	INSERT INTO stg.Errors (StgID, UserID, FullName, Age, Email, RegistrationDate, LastLoginDate, PurchaseTotal)
-	SELECT
-		StgID, UserID, FullName, Age, Email, RegistrationDate, LastLoginDate, PurchaseTotal
-	FROM stg.Users
-	WHERE
-		-- Add records with null values.
-		UserID IS NULL
-		OR FullName IS NULL
-		OR Age IS NULL
-		OR Email IS NULL
-		OR RegistrationDate IS NULL
-		OR LastLoginDate IS NULL
-		OR PurchaseTotal IS NULL
-		-- Add records with special characters.
-		OR FullName LIKE '%[^a-zA-Z0-9 ]%'
-		OR Email LIKE '%[^a-zA-Z0-9@._-]%'
-		-- Add records with future dates.
-		OR RegistrationDate > GETDATE()
-		OR LastLoginDate > GETDATE()
-		OR RegistrationDate > LastLoginDate
-		-- Add records with negative values.
-		OR UserID < 0
-		OR Age < 0
-		OR PurchaseTotal < 0
-		-- Add records with invalid email format.
-		OR Email NOT LIKE '%_@__%.__%'
-		-- Add records with very old dates.
-		OR RegistrationDate < DATEADD(YEAR, -100, GETDATE())
-		OR LastLoginDate < DATEADD(YEAR, -100, GETDATE())
-		-- Add records with outlier purchase totals.
-		OR PurchaseTotal < @LowerLimit
-		OR PurchaseTotal > @UpperLimit;
-
-	-- DELETE records with Staging Table
+	-- DELETE records from Staging Table and capture the delted records using OUTPUT
 	DELETE FROM stg.Users
+	OUTPUT
+		DELETED.StgID,
+		DELETED.UserID,
+		DELETED.FullName,
+		DELETED.Age,
+		DELETED.Email,
+		DELETED.RegistrationDate,
+		DELETED.LastLoginDate,
+		DELETED.PurchaseTotal
+	INTO
+		stg.Errors
 	WHERE
 		-- Remove records with null values.
 		UserID IS NULL
@@ -120,6 +97,6 @@ BEGIN
 		OR LastLoginDate < DATEADD(YEAR, -100, GETDATE())
 		-- Remove records with outlier purchase totals.
 		OR PurchaseTotal < @LowerLimit
-		OR PurchaseTotal > @UpperLimit;
+		OR PurchaseTotal > @UpperLimit
 END
 GO
